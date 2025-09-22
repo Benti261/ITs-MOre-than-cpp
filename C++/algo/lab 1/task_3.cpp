10b70963708e9fcd6412d2eac0da72283ad6c3e6
Vlad Krivulets <102356010+Benti261@users.noreply.github.com>
2025-09-22 14:35:52 +0300

    task_3.cpp no string/vector

--- C++/algo/lab 1/task_3.cpp
+++ C++/algo/lab 1/task_3.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
-#include <vector>
-#include <string>
 using namespace std;
 
-int count(string word, char let) {
-    int len = word.size();
+int count_char(const char *word, char let) {
     int ans = 0;
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; word[i] != '\0'; i++) {
         if (word[i] == let) {
             ans++;
         }
@@ -15,12 +12,12 @@ int count(string word, char let) {
 }
 
 int main() {
-    vector<char> alph = {'z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r', 'q', 'p', 'o', 'n', 'm', 'l', 'k', 'j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a'};
-    string code;
+    char alph[26] = {'z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r', 'q', 'p', 'o', 'n', 'm', 'l', 'k', 'j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a'};
+    char code[1001];
     cin >> code;
     
     for (int i = 0; i < 26; i++) {
-        if (count(code, alph[i]) > 0) {
+        if (count_char(code, alph[i]) > 0) {
             cout << alph[i] << endl;
             return 0;
         }
