4de96396af39c58fda0b2daf403b2c69e4df3809
Vlad Krivulets <102356010+Benti261@users.noreply.github.com>
2025-10-03 16:39:18 +0300

    Add files via upload

---
+++ OpLabs/OpLab_1/build/CMakeFiles/generator.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/benti/Desktop/c++/OpLabs/OpLab1/labwork1-Benti261/data.cpp" "CMakeFiles/generator.dir/data.cpp.o" "gcc" "CMakeFiles/generator.dir/data.cpp.o.d"
+  "/home/benti/Desktop/c++/OpLabs/OpLab1/labwork1-Benti261/errors.cpp" "CMakeFiles/generator.dir/errors.cpp.o" "gcc" "CMakeFiles/generator.dir/errors.cpp.o.d"
+  "/home/benti/Desktop/c++/OpLabs/OpLab1/labwork1-Benti261/input.cpp" "CMakeFiles/generator.dir/input.cpp.o" "gcc" "CMakeFiles/generator.dir/input.cpp.o.d"
+  "/home/benti/Desktop/c++/OpLabs/OpLab1/labwork1-Benti261/main.cpp" "CMakeFiles/generator.dir/main.cpp.o" "gcc" "CMakeFiles/generator.dir/main.cpp.o.d"
+  "/home/benti/Desktop/c++/OpLabs/OpLab1/labwork1-Benti261/template.cpp" "CMakeFiles/generator.dir/template.cpp.o" "gcc" "CMakeFiles/generator.dir/template.cpp.o.d"
+  "" "generator" "gcc" "CMakeFiles/generator.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ OpLabs/OpLab_1/build/CMakeFiles/generator.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/generator.dir/link.d"
+  "CMakeFiles/generator.dir/data.cpp.o"
+  "CMakeFiles/generator.dir/data.cpp.o.d"
+  "CMakeFiles/generator.dir/errors.cpp.o"
+  "CMakeFiles/generator.dir/errors.cpp.o.d"
+  "CMakeFiles/generator.dir/input.cpp.o"
+  "CMakeFiles/generator.dir/input.cpp.o.d"
+  "CMakeFiles/generator.dir/main.cpp.o"
+  "CMakeFiles/generator.dir/main.cpp.o.d"
+  "CMakeFiles/generator.dir/template.cpp.o"
+  "CMakeFiles/generator.dir/template.cpp.o.d"
+  "generator"
+  "generator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/generator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
