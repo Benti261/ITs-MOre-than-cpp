4fe42efcda2bace887e0993244c20f574270ed4e
Vlad Krivulets <102356010+Benti261@users.noreply.github.com>
2025-10-19 20:06:52 +0300

    Update number.h

--- OpLabs/OpLab_2/lib/number.h
+++ OpLabs/OpLab_2/lib/number.h
@@ -2,27 +2,20 @@
 #include <cinttypes>
 #include <iostream>
 
-
 struct int2025_t {
-    // implement me
+    static const int SIZE = 253;
+    uint8_t data[SIZE];
 };
 
-static_assert(sizeof(int2025_t) <= 254,  "Size of int2025_t must be no higher than 254 bytes");
+static_assert(sizeof(int2025_t) <= 254, "Size of int2025_t must be no higher than 254 bytes");
 
 int2025_t from_int(int32_t i);
-
 int2025_t from_string(const char* buff);
-
 int2025_t operator+(const int2025_t& lhs, const int2025_t& rhs);
-
 int2025_t operator-(const int2025_t& lhs, const int2025_t& rhs);
-
 int2025_t operator*(const int2025_t& lhs, const int2025_t& rhs);
-
 int2025_t operator/(const int2025_t& lhs, const int2025_t& rhs);
-
+int2025_t operator%(const int2025_t& lhs, const int2025_t& rhs);
 bool operator==(const int2025_t& lhs, const int2025_t& rhs);
-
 bool operator!=(const int2025_t& lhs, const int2025_t& rhs);
-
 std::ostream& operator<<(std::ostream& stream, const int2025_t& value);
